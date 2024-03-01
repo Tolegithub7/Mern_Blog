b363aa8e7170c0196664da0bd8a78a457396f216
Tolegithub7 <Tolekadima7@gmail.com>
2024-03-01 22:59:51 +0300

    Creating more pages and implementing route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Article from "./Pages/Article";
+import ArticleList from "./Pages/ArticleList";
 
 function App() {
   return (
@@ -7,6 +10,9 @@ function App() {
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/article" element={<Article />} />
+          <Route path="/article-list" element={<ArticleList />} />
         </Routes>
       </div>
     </Router>
