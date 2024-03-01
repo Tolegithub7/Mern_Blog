abc6e8f42841d78df0eb0ace2638308e127207ab
Tolegithub7 <Tolekadima7@gmail.com>
2024-03-01 23:38:34 +0300

    Adding NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+//pages
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Article from "./Pages/Article";
 import ArticleList from "./Pages/ArticleList";
 
+//components
+import Navbar from "./Components/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/article" element={<Article />} />
-          <Route path="/article-list" element={<ArticleList />} />
+          <Route path="/articles-list" element={<ArticleList />} />
         </Routes>
       </div>
     </Router>
