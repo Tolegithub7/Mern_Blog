f518caf178cd7d801dde6a85e74c932749939063
Tolegithub7 <Tolekadima7@gmail.com>
2024-03-01 22:34:41 +0300

    tailwindcss installed and integrated and Home page for temporarly

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        {" "}
+        WelCome to my Blog
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Aute minim magna velit ut occaecat occaecat. Non sunt est laboris
+        consectetur ut est velit. Aliqua Lorem eiusmod deserunt et laborum non
+        reprehenderit incididunt voluptate sint in culpa est do.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Enim aute do dolore id magna proident nulla laboris amet deserunt quis
+        esse sint magna. Irure officia commodo quis laborum irure officia
+        eiusmod veniam excepteur cupidatat anim. Ea aute elit quis Lorem dolore
+        ipsum. Pariatur consequat est sit eiusmod voluptate est ea.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Velit laborum velit dolore nisi sunt anim dolor sunt ad Lorem tempor
+        excepteur est ad. Occaecat et est est nostrud velit. Esse occaecat
+        veniam ipsum aliquip esse laboris incididunt ut aute nisi sit minim
+        ipsum nulla. Nisi fugiat anim deserunt aliqua ea velit excepteur
+        excepteur. Ad laborum elit ex amet irure culpa deserunt quis est non
+        fugiat est velit. In qui fugiat mollit tempor cupidatat nostrud. Dolor
+        reprehenderit ex sunt non in aliquip sit sunt elit nulla. Anim irure
+        Lorem amet pariatur aute amet commodo. Amet amet pariatur incididunt
+        consectetur cillum aute velit est. Lorem laborum laborum pariatur
+        voluptate Lorem minim ipsum aute quis enim cillum deserunt id officia.
+        Irure magna cillum magna deserunt esse non sit sint Lorem quis
+        excepteur. Anim sunt velit ea esse amet deserunt esse esse ullamco
+        proident eu exercitation sunt pariatur. Cillum deserunt ipsum pariatur
+        duis irure elit enim.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
